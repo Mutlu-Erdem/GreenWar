1fca4ee2edf1a81f8494621247362fed308d1533
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-08-10 22:38:02 +0300

    Camera stops on leftboundry dynamically

--- Assets/_Scripts/CameraMovement.cs
+++ Assets/_Scripts/CameraMovement.cs
@@ -2,23 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
-{
+public class CameraMovement : MonoBehaviour{
 
-	public GameObject tracker;
-	// Use this for initialization
-	void Start ()
-	{
+    public GameObject leftEdge;
+    public GameObject rightEdge;
 
-	}
-	
-	// Follow player within limited X and Y axises.
-	void FixedUpdate ()
-	{
-		transform.position = new Vector3 (Mathf.Clamp(tracker.transform.position.x, -360, 8), Mathf.Clamp(tracker.transform.position.y, -80, -50), transform.position.z);
+    private float _leftEdgeXLocation;
+    private float _rightEdgeXLocation;
+    private float _cameraBoundry;
 
-		/* Debug for main camera position !!! NOT TRACER /*
-		//print ("camera position: " + transform.position.ToString ());
-		*/
-	}
+    public GameObject tracker;
+    // Use this for initialization
+    void Start(){
+        //TODO implement comment for feature.
+        _leftEdgeXLocation = leftEdge.transform.position.x;
+        _rightEdgeXLocation = rightEdge.transform.position.x;
+
+        // Make sure camera starts from left boundry.
+        _cameraBoundry = transform.position.x - _leftEdgeXLocation;
+
+        _leftEdgeXLocation += _cameraBoundry;
+        _rightEdgeXLocation -= _cameraBoundry;
+    }
+
+    // Follow player within limited X and Y axises.
+    void FixedUpdate(){
+        transform.position =
+            new Vector3(Mathf.Clamp(tracker.transform.position.x, _leftEdgeXLocation, _rightEdgeXLocation),
+                Mathf.Clamp(tracker.transform.position.y, -80, -50), transform.position.z);
+
+
+
+        /* Debug for main camera position !!! NOT TRACER /*
+        //print ("camera position: " + transform.position.ToString ());
+        */
+    }
+
 }
