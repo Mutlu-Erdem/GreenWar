0984d332b4922e76969421a87c5520080320fbc4
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-08-10 23:04:26 +0300

    MovingPlatform going left and right only.

---
+++ Assets/_Scripts/MovingPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour{
+
+    public float distance = 75;
+
+    private float _startingLocation;
+    private bool _isgoingLeft;
+
+    // Use this for initialization
+    void Start (){
+        _startingLocation = transform.position.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (_isgoingLeft) {
+	        transform.Translate(Vector3.left);
+	    }
+	    else {
+	        transform.Translate(Vector3.right);
+	    }
+      //  print("transform : " + transform.position.x + " starting location: " + _startingLocation);
+	    if (transform.position.x - _startingLocation > distance) {
+	        print("asdasd");
+	        _isgoingLeft = true;
+	    }
+
+	    if (transform.position.x - _startingLocation < -distance) {
+	        print("-----dasdasd");
+            _isgoingLeft = false;
+        }
+
+
+    }
+}
